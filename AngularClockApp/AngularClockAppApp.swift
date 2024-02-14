75f04ac045c0c30f08ffbe66c1426a5b02da9a76
pouyasadri <pouyapaghandeh78@gmail.com>
2024-02-14 14:37:27 +0100

    Initial Commit

---
+++ AngularClockApp/AngularClockAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AngularClockAppApp.swift
+//  AngularClockApp
+//
+//  Created by Pouya Sadri on 14/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct AngularClockAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
